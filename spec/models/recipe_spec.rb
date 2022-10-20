c3010c2735e55475bd95b77a68023d5dde56fa29
Fisola Adesuyi <adesuyifisola@gmail.com>
2022-10-20 16:35:05 +0200

    rspec test for model validation

--- spec/models/recipe_spec.rb
+++ spec/models/recipe_spec.rb
@@ -8,4 +8,8 @@ RSpec.describe Recipe, type: :model do
   describe "description" do
     it { is_expected.to validate_presence_of(:description) }
   end
+
+  # it "is boolean" do
+  #   expect(subject).to validate_inclusion_of(:completed).in_array([true, false])
+  # end
 end
