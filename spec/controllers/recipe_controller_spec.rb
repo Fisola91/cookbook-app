4979be95cf09b47b8de7612fd58cdba7d32c4cbc
Fisola Adesuyi <adesuyifisola@gmail.com>
2022-10-25 01:21:37 +0200

    added factory bot and faker gems to the gem file and used it to test models and code

--- spec/controllers/recipe_controller_spec.rb
+++ spec/controllers/recipe_controller_spec.rb
@@ -4,17 +4,18 @@ RSpec.describe RecipesController, type: :controller do
   def app
     RecipesController
   end
-  context "GET #index" do
+  describe "GET #index" do
     it "returns a success response" do
       get "/"
       expect(last_response.status).to eq(200)
     end
+
+    # it "displays all recipes" do
+    #   recipe = Recipe.all
+    #   get "/"
+    #   expect(last_response.body).to eq(recipe)
+    # end
+
   end
 
-  # context "all recipes" do
-  #   let(:response) { get "/" }
-  #   it "displays all recipes" do
-  #     expect(response.body).to eq(Recipe.all)
-  #   end
-  # end
 end
