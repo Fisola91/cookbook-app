bea45f501a10308d9869312e9cec8246c9531575
Fisola Adesuyi <adesuyifisola@gmail.com>
2022-10-25 01:25:49 +0200

    rspec test passed for model

--- spec/models/recipe_spec.rb
+++ spec/models/recipe_spec.rb
@@ -4,26 +4,22 @@ RSpec.describe Recipe, type: :model do
   context "validation test" do
     let(:recipe) { build(:random_recipe)}
     it "should save successfully" do
-      # recipe = Recipe.new(name: "Tomato", description: "rice, tomat", completed: "nil").save
       expect(recipe.save).to eq(true)
     end
 
     it "ensures the presence of recipe name" do
-      # recipe = Recipe.new(description: "rice, tomat", completed: "nil").save
       recipe.name = nil
       expect(recipe.save).to eq(false)
     end
 
     it "ensures the presence of recipe description" do
-      # recipe = Recipe.new(name: "Tomato", completed: "nil").save
       recipe.description = nil
       expect(recipe.save).to eq(false)
     end
 
     it "ensures the presence of completed recipe" do
-      # recipe = Recipe.new(name: "Tomato", description: "rice, tomat").save
-      recipe.completed = true
-      expect(recipe.save).to eq(false)
+      recipe.completed = nil
+      expect(recipe.save).to eq(true)
     end
 
   end
