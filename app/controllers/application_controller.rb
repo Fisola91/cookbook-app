b4704ee62bbbcf2aa8b69dca44bf8c2caa4a17e8
Fisola Adesuyi <adesuyifisola@gmail.com>
2022-10-24 22:31:53 +0200

    test for response status is when user visit the index page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,10 +6,10 @@ class ApplicationController < Sinatra::Base
   configure :development do
     # use BetterErrors::Middleware
     # BetterErrors.application_root = File.expand_path(__dir__)
-    set :public_folder, 'public'
-    set :views, 'app/views/recipes'
+
     register Sinatra::Reloader
   end
-
+  set :public_folder, 'public'
+  set :views, 'app/views/recipes'
 
 end
